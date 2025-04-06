bb3004ca95266262fe60cb36cf64b3ca129cae5a
camilo007 <javier.kamilo007@gmail.com>
2025-04-06 13:58:00 -0500

    Se realiza ajuste enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyARvgpLaiF3jtwkbtePbqHOEUey_U2huQg",
+    authDomain: "todotask-aeef1.firebaseapp.com",
+    projectId: "todotask-aeef1",
+    storageBucket: "todotask-aeef1.appspot.com",
+    messagingSenderId: "666328114646",
+    appId: "1:666328114646:android:f0971483c1d0d288fdf077"
+  }
 };
